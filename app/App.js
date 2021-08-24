e03273949feb7e22e2e44efabfc3f983a347133c
Carlos1205 <carloslima68.cs@gmail.com>
2021-08-24 02:20:32 -0300

    Tirando arquivos

--- app/App.js
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Login from './views/Login';
-
-export default function App() {
-  return (
-      <Login />
-    );
-}
